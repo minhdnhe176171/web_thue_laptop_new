1338b7440f549da5582353856515e961f442c58c
anhch <anhchhe150287@fpt.edu.vn>
2025-12-17 09:32:46 +0700

    update

--- web_chothue_laptop/Models/Customer.cs
+++ web_chothue_laptop/Models/Customer.cs
@@ -23,6 +23,8 @@ public partial class Customer
 
     public DateTime? CreatedDate { get; set; }
 
+    public bool BlackList { get; set; }
+
     public virtual ICollection<BookingReceipt> BookingReceipts { get; set; } = new List<BookingReceipt>();
 
     public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
